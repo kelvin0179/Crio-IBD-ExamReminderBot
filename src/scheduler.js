f81208489d1a5c9f8a3e113f67a2b89899785c90
Mohit Jaiswal <mohitjaiswal819@gmail.com>
2021-07-05 07:44:42 +0530

    Cron config

--- src/scheduler.js
+++ src/scheduler.js
@@ -7,7 +7,7 @@ module.exports = (client) => {
     client.on('ready', () => {
         channel = client.channels.cache.find(iterator => iterator.name === "general");
     });
-    var reminderString = "0 7 */1 * *", deleteString = "0 23 */1 * *";
+    var reminderString = "0 7 */1 * *", deleteString = "59 23 */1 * *";
     // var reminderString = "*/10 * * * * *", deleteString = "*/15 * * * * *";
     cron.schedule(deleteString, async (req, res) => {
         console.log("Deletion Job Active");
